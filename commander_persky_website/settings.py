e3b796b92357d758187c2128ad8b805011a3a47d
DanielOr18 <daniel.or18@gmail.com>
2024-12-10 18:34:31 +0200

    Fix import statements in settings.py

--- commander_persky_website/settings.py
+++ commander_persky_website/settings.py
@@ -23,7 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-import os
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'your-very-secret-key-here')
 
 # SECURITY WARNING: don't run with debug turned on in production!
